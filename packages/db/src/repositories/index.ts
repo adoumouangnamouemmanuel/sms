90df9d826a4dbbfefee53ff86cc11d79e6e8b110
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-08-13 21:40:15 +0000

    chore(db): export modular repositories

---
+++ packages/db/src/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './audit-log.repository';
+export * from './base';
+export * from './refresh-session.repository';
+export * from './school.repository';
+export * from './user.repository';
