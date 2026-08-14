0c49c299e5f44dd693fdc175edaf95c08527ba7b
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-08-14 00:39:58 +0000

    chore(web-auth): export auth recovery hooks

--- apps/web/src/modules/auth/index.ts
+++ apps/web/src/modules/auth/index.ts
@@ -2,5 +2,7 @@ export * from './authApi';
 export * from './authErrors';
 export * from './authRuntime';
 export * from './authSession';
+export * from './ForgotPasswordNotice';
 export * from './LoginScreen';
 export * from './useLoginForm';
+export * from './useLogoutAction';
