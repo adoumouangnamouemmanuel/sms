058ac3a9da8776807e74f8a0ab4caf3a1cdcc482
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-08-14 00:39:56 +0000

    fix(api): package bcrypt sidecar assets

--- apps/api/pkg.sidecar.config.cjs
+++ apps/api/pkg.sidecar.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   assets: [
     'node_modules/better-sqlite3/prebuilds/win32-x64.node',
+    'node_modules/bcrypt/package.json',
+    'node_modules/bcrypt/prebuilds/win32-x64/bcrypt.node',
     '../../packages/db/migrations/sqlite/**/*',
   ],
-  publicPackages: ['better-sqlite3'],
+  publicPackages: ['bcrypt', 'better-sqlite3'],
 };
