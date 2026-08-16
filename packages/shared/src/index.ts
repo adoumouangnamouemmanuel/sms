8ad434670df4681e2aed9e46d6e116c01fe5675f
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-08-16 19:10:17 +0000

    feat(shared): export the configuration module types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from './auth.js';
 export * from './app.js';
 export * from './audit.js';
 export * from './classes.js';
+export * from './configuration.js';
 export * from './imports.js';
 export * from './logging.js';
 export * from './people.js';
